5d76a07a339c35e8af9b8ef0d6e0604619a00bbb
Marcus Vinicius Nascimento Ferreira <dev.mvinicius@gmail.com>
2026-03-29 18:23:45 -0300

    feat: Refactor book routes to use BookController and Prisma for database operations

--- src/routes.ts
+++ src/routes.ts
@@ -1,68 +1,17 @@
-import express from 'express'
-import { DATABASE_BOOKS } from './repositories/database'
-import { loggerMiddleware } from './middlewares/middleware'
+import express, { Request, Response } from 'express'
+import { prisma } from './lib/prisma'
+import { BookService } from './services/book-service'
+import { BookRepository } from './repositories/book-repository'
+import { BookController } from './controllers/book-controllers'
 
 const router = express.Router()
 
-router.use(loggerMiddleware)
+const bookRepository = new BookRepository(prisma)
+const bookService = new BookService(bookRepository)
+const bookController = new BookController(bookService)
 
-router.post('/book', (req, res) => {
-  const { title, year } = req.body
-
-  const newBook = {
-    id: DATABASE_BOOKS.length + 1,
-    title,
-    year,
-  }
-
-  DATABASE_BOOKS.push(newBook)
-
-  res.status(201).json(newBook)
+router.post('/book', (req: Request, res: Response) => {
+  bookController.createBook(req, res)
 })
 
-router.get('/book', (req, res) => {
-  const year = Number(req.query.year)
-
-  if (year) {
-    const filteredBooks = DATABASE_BOOKS.filter((b) => b.year === year)
-    return res.status(200).json(filteredBooks)
-  }
-
-  res.status(200).json(DATABASE_BOOKS)
-})
-
-router.get('/book/:id', (req, res) => {
-  const { id } = req.params
-  const book = DATABASE_BOOKS.find((b) => b.id === parseInt(id))
-
-  if (!book) {
-    return res.status(404).json({ messyear: 'Book not found' })
-  }
-
-  res.status(200).json(book)
-})
-
-router.delete('/book/:id', (req, res) => {
-  const { id } = req.params
-  const bookIndex = DATABASE_BOOKS.findIndex((b) => b.id === parseInt(id))
-  if (bookIndex === -1) {
-    return res.status(404).json({ messyear: 'Book not found' })
-  }
-  DATABASE_BOOKS.splice(bookIndex, 1)
-  res.status(200).json({ messyear: 'Book deleted' })
-})
-
-router.patch('/book/:id', (req, res) => {
-  const { id } = req.params
-  const { title } = req.body
-  const book = DATABASE_BOOKS.find((b) => b.id === parseInt(id))
-
-  if (!book) {
-    return res.status(404).json({ messyear: 'Book not found' })
-  }
-  if (title) {
-    book.title = title
-  }
-  res.status(200).json(book)
-})
 export default router
