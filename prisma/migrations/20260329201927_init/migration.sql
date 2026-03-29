3657f662e78e0c8da3f5b987c0c2ad52105d8ba9
Marcus Vinicius Nascimento Ferreira <dev.mvinicius@gmail.com>
2026-03-29 17:27:25 -0300

    feat: Add Prisma, Migration and Schema
    commit 07229f7ef6696a6d364d36dbeaf3a22f0bc34bc2
    Author: Marcus Vinicius Nascimento Ferreira <dev.mvinicius@gmail.com>
    Date:   Sun Mar 29 17:27:09 2026 -0300
    
        feat: add Prisma setup with initial Book model and database migration
    
        - Updated package.json to include Prisma dependencies and dotenv for environment variable management.
        - Created prisma.config.ts for Prisma configuration.
        - Added initial migration SQL for creating the Book table with relevant fields and constraints.
        - Introduced migration lock file for version control.
        - Defined Prisma schema with Book model including fields for id, title, isbn, publishedAt, createdAt, updatedAt, and author.

---
+++ prisma/migrations/20260329201927_init/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE `Book` (
+    `id` VARCHAR(191) NOT NULL,
+    `title` VARCHAR(191) NOT NULL,
+    `isbn` VARCHAR(191) NULL,
+    `publishedAt` INTEGER NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    `author` VARCHAR(191) NOT NULL,
+
+    UNIQUE INDEX `Book_isbn_key`(`isbn`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
