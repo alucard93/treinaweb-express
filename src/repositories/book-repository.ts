746534202c9dd83c49f6aa265489f98d7c421773
Marcus Vinicius Nascimento Ferreira <dev.mvinicius@gmail.com>
2026-03-30 15:01:20 -0300

    fix: Update Prisma import paths in prisma.ts and book-repository.ts

--- src/repositories/book-repository.ts
+++ src/repositories/book-repository.ts
@@ -1,4 +1,4 @@
-import { Prisma, PrismaClient } from '../../generated/prisma/client'
+import { Prisma, PrismaClient } from '../../generated/prisma'
 
 export class BookRepository {
   constructor(private prisma: PrismaClient) {}
